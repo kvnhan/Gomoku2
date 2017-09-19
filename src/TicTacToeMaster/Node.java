75c944e44c28136aab5df9e7d7c7a223536c2023
jmetzger <jmetzger@fe80:0:0:0:947f:37ff:fed8:ec38%9>
2017-09-19 15:19:23 -0400

    x

---
+++ src/TicTacToeMaster/Node.java
@@ -0,0 +1,31 @@
+package TicTacToeMaster;
+
+public class Node {
+	String[][] board = new String[3][3];
+	String nextPlayer;
+	Node parent;
+	int heuristicValue;
+	int atDepth;
+	
+	public Node() {}
+	public Node(String[][] board, Node parent, int heuristicValue, int atDepth, String nextPlayer) {
+		this.board = board;
+		this.nextPlayer = nextPlayer;
+		this.parent = parent;
+		this.heuristicValue = heuristicValue;
+		this.atDepth = atDepth;
+	}
+	public Node(String[][] board) {
+		this(board, null, 0, 0, null);
+	}
+	public Node initializeNode() {
+		return new Node(); 
+	}
+	public Node initializeNodeWithInput(String[][] board) {
+		Node root = new Node();
+		root.board = board;
+		root.nextPlayer = "X";	//Always initialize machine as X node.
+		return root;
+	}
+	
+}
